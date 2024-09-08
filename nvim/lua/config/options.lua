d795a0b1df005d5874e3271105d0df5d9c2ec371
LucasBeastBeast <nikal.gurung@gmail.com>
2024-09-08 14:25:39 +0545

    Set tokyonight theme in nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,8 +9,9 @@ vim.o.relativenumber = true
 -- Clipboard
 vim.o.clipboard = 'unnamedplus'
 
--- Python Provider
-vim.g.python3_host_prog = '~/myenv/bin/python3'
+-- Colorscheme
+vim.cmd[[colorscheme tokyonight]]
 
 -- Transparent background
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+
